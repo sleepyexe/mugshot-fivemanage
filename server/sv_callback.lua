73ed6f05d035c3d608640858f9cd0e5d87d30315
Sleepy Rae <67649181+sleepyexe@users.noreply.github.com>
2024-08-29 21:44:36 +0700

    Update sv_callback.lua

--- server/sv_callback.lua
+++ server/sv_callback.lua
@@ -1,7 +1,5 @@
-local root = GetResourcePath(GetCurrentResourceName())
 local apiKey = GetConvar('FIVEMANAGE_MEDIA_API_KEY', '')
 lib.callback.register('getMugshotUrl', function (playerid, mugshot)
     local path = exports.mugshot:ConvertBase64(mugshot, apiKey)
     return path
-    -- return mugshot
 end)
